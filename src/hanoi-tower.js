82df970317cac4154b7267ebc17217dba6637faf
fizik003 <kassik0boks@gmail.com>
2020-02-23 02:51:08 +0300

    fest: comleted hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turn_count = 2 ** disksNumber -1;
+    let turn_speed_sec = turnsSpeed / 3600;
+    let sec = turn_count / turn_speed_sec;
+    return {turns: turn_count, seconds: sec};
+
+
+    
 }
